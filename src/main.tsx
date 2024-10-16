6ad21aaed25e9a6432b1238c730de56b6cc50d45
MrBJasak <mrbjasak@gmail.com>
2024-10-17 00:26:57 +0200

    Refactor main.tsx to remove unused Inspector component

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react';
-import { Inspector } from 'react-dev-inspector';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './styles/styles.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Inspector />
+    {/* <Inspector /> */}
     <App />
   </StrictMode>,
 );
