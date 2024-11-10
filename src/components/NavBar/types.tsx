5bcf5adc1f67371f6e1b17815d5f04b7770acb8b
MrBJasak <mrbjasak@gmail.com>
2024-11-10 11:59:41 +0100

    remove utils

--- src/components/NavBar/types.tsx
+++ src/components/NavBar/types.tsx
@@ -24,15 +24,3 @@ export const navItems = [
   { label: 'Usługi', path: '/zakres-uslug' },
   { label: 'Kontakt', path: '/kontakt' },
 ];
-
-const utils = {
-  title: `Szanowni Państwo,
-serdecznie witam na stronie mojej Kancelarii Adwokackiej.
-7 listopada minie dokładnie miesiąc odkąd, w ramach jednoosobowej działalności gospodarczej jako adwokat aktywnie wykonujący zawód, rozpoczęłam prowadzenie własnej Kancelarii Adwokackiej.
-Moim priorytetem jest indywidualne podejście do klienta oraz dbałość o jego interesy. Gwarantuję uważne wsłuchanie się w Państwa problemy, potrzeby i pytania.
-Angażując się w każdą sprawę indywidualnie, przewiduje jej ewentualny przebieg, proponując skuteczne rozwiązania.
-W ramach swoich usług oferuję obsługę klientów indywidualnych, podmiotów gospodarczych i organizacji.
-Moja praktyka obejmuje sprawy z zakresu: prawa cywilnego, prawa rodzinnego, prawa własności intelektualnej, prawa pracy, prawa gospodarczego, prawa upadłościowego (w tym sprawy o ogłoszenie upadłości konsumenckiej), prawa administracyjnego i prawa karnego.
-Nieustannie staram się pogłębiać swoją wiedzę.
-Mam nadzieję, że obdarzą Państwo moją Kancelarię Adwokacką zaufaniem, powierzając swoją sprawę do prowadzenia.`
-}
